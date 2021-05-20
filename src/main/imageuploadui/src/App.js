7627ae5faa26c5e7fe2ec7b323e8f7ae4b4d98af
ddoshi2021 <ddoshi2021@gmail.com>
2021-05-20 16:35:15 -0700

    Add UserProfiles Component clean up manifest

--- src/main/imageuploadui/src/App.js
+++ src/main/imageuploadui/src/App.js
@@ -1,11 +1,27 @@
 import React, {useState, useEffect} from "react";
 import './App.css';
-import axios from "axios"
+import axios from 'axios'
 
+const UserProfiles = () => {
+  const fetchUserProfiles =() => {
+    axios.get("http://localhost:8080/api/v1/user-profile")
+    .then(resp => {
+      console.log(resp);
+    })
+  }
+
+  useEffect(() => {
+    fetchUserProfiles();
+  }, [])
+
+  return <h1>Hello Image</h1>
+}
 
 function App() {
   return (
-    <div className="App"></div>
+    <div className="App">
+      <UserProfiles />
+    </div>
   );
 }
 
