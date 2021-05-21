dfd0ef08960fedbeb6228e6f7ddd942b49dd278e
ddoshi2021 <ddoshi2021@gmail.com>
2021-05-20 17:59:08 -0700

    add hook usestate useEffect

--- src/main/imageuploadui/src/Component/UserProfiles.js
+++ src/main/imageuploadui/src/Component/UserProfiles.js
@@ -2,22 +2,28 @@ import React, {useState, useEffect} from "react";
 import axios from 'axios'
 
 function UserProfiles(props) {
+
+    const [userProfiles, setUserProfiles] = useState([]);
+
     const fetchUserProfiles =() => {
         axios.get("http://localhost:8080/api/v1/user-profile")
         .then(resp => {
           console.log(resp);
-        })
+          setUserProfiles(resp.data);
+        });
       }
     
       useEffect(() => {
         fetchUserProfiles();
       }, [])
 
-    return (
-        <React.Fragment>
-            <h1>Hello Image</h1>
-        </React.Fragment>
-    )
+    return userProfiles.map((userProfile, index) => {
+      return (
+      <div key={index}>
+          <h3>{userProfile.userName}</h3>
+           <p>{userProfile.userProfileID}</p>
+      </div>)
+    })
 }
 
 export default UserProfiles
