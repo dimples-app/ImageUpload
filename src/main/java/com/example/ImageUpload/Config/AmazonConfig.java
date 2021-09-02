022f422d897535818768189854ee443e453de20a
ddoshi2021 <ddoshi2021@gmail.com>
2021-09-02 11:20:28 -0700

    created yml

--- src/main/java/com/example/ImageUpload/Config/AmazonConfig.java
+++ src/main/java/com/example/ImageUpload/Config/AmazonConfig.java
@@ -7,18 +7,25 @@ import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AmazonConfig {
 
+    @Value("${S3.accessKey}")
+    private String accessKey;
+
+    @Value("${S3.secretKey}")
+    private String secretKey;
+
+
     @Bean
     public AmazonS3 s3() {
         AWSCredentials awsCredentials = new BasicAWSCredentials(
-                "AKIARQPPM7BNRHWHPJXY",
-                "3W5oKXePS2gpr+maI0Dpt0En+HXPcdzu8dBRwezm"
-
+                accessKey,
+                secretKey
         );
         return AmazonS3ClientBuilder
                 .standard()
