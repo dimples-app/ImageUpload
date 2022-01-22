2e1ed4f59cdd7d406b6b8578e5793d390e5756b7
dimple <13650125+dimples-app@users.noreply.github.com>
2022-01-22 13:37:25 -0700

    Create AmazonConfig.java

---
+++ src/main/java/com/example/ImageUpload/Config/AmazonConfig.java
@@ -0,0 +1,39 @@
+package com.example.ImageUpload.Config;
+
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AmazonConfig {
+
+    @Value("${S3.accessKey}")
+    private String accessKey;
+
+    @Value("${S3.secretKey}")
+    private String secretKey;
+
+    @Value("${S3.region}")
+    private String region;
+
+    @Bean
+    public AmazonS3 s3() {
+        AWSCredentials awsCredentials = new BasicAWSCredentials(
+                accessKey,
+                secretKey
+        );
+        return AmazonS3ClientBuilder
+                .standard()
+                .withRegion(region)
+                .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
+                .build();
+    }
+}
+© 2022 GitHub, Inc.
